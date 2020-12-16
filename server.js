7496df20035d0257722055f7f1b454a9b8ce1801
AdioleDivine <adioledivine27@Gmail.com>
2020-12-17 00:31:49 +0100

    Fix Routes

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
 const bodyParser = require('body-parser')
-const routes = require('../Universities/routes/index')  
+const routes = require('./routes/index')  
 const dotenv = require('dotenv');
 
 
